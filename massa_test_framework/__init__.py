b24218880f4aebf9b1960ddc854ec04630520be8
sydhds <sydhds@gmail.com>
2023-09-27 09:49:40 +0200

    Move k8s files in sub dir

--- massa_test_framework/__init__.py
+++ massa_test_framework/__init__.py
@@ -1,11 +1,10 @@
-from .server import Server, ServerOpts
+from .server import Server, ServerOpts, MassaNodeOpts
 from .node import Node
 from .compile import CompileOpts, CompileUnit
 from .ledger_editor import LedgerEditor
 
 # k8s manager
-from .kubernetes_manager import KubernetesManager
-
+# from .kubernetes_manager import KubernetesManager
 
 # data
 from .misc import node_keys_list, NodeKeys
