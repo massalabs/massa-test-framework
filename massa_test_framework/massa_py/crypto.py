74e8717ab9f330f1c36c5d352d121f8e4d5ea2d7
sydhds <sydhds@gmail.com>
2023-11-08 17:41:06 +0100

    Use PyNaCL instead of unmaintained ed25519 lib

--- massa_test_framework/massa_py/crypto.py
+++ massa_test_framework/massa_py/crypto.py
@@ -5,18 +5,20 @@ import base58
 # import random
 from blake3 import blake3
 import varint
-import ed25519
-
+from nacl.signing import SigningKey, VerifyKey
 
 class KeyPair:
-    def __init__(self, secret_key=None, public_key=None):
+    def __init__(self, secret_key: SigningKey, public_key: VerifyKey):
         self.secret_key = secret_key
         self.public_key = public_key
 
-    def random(self):
-        signing_key, verifying_key = ed25519.create_keypair()
-        return KeyPair(secret_key=signing_key, public_key=verifying_key)
+    @staticmethod
+    def random():
+        sk = SigningKey.generate()
+        vk = sk.verify_key
+        return KeyPair(secret_key=sk, public_key=vk)
 
+    @staticmethod
     def from_secret_massa_encoded(private: str):
         # Strip identifier
         private = private[1:]
@@ -27,8 +29,12 @@ class KeyPair:
         # Get rest (for the moment versions are little)
         secret_key_ = private[1:]
         # decode privkey
-        secret_key = ed25519.keys.SigningKey(secret_key_)
-        public_key = secret_key.get_verifying_key()
+        # PyNaCL can only init SigningKey object from seed
+        # From https://github.com/pyca/pynacl/issues/639 and crypto_sign_ed25519_sk_to_seed function code
+        # the seed is in fact the private key
+        seed = secret_key_
+        secret_key = SigningKey(seed)
+        public_key = secret_key.verify_key
         return KeyPair(secret_key=secret_key, public_key=public_key)
 
     def get_public_massa_encoded(self):
